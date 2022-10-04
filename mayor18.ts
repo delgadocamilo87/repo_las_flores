76679447e4cabf3cec44a43ee789f0f55d14df81
“usuariogit config --global user.name “delgado.camilo87 <usuario@example.com>
2022-10-04 19:47:33 -0300

    primer commit

--- mayor18.ts
+++ mayor18.ts
@@ -1,7 +1,11 @@
 let readlineSync: any = require('readline-sync');
 let nroDeseado = readlineSync.question('Escriba el numero que desea verificar si es mayor o no a 18: ');
-if (nroDeseado > 20) {
+    if (nroDeseado > 20) {
     console.log('El número es mayor a 18: ' + nroDeseado);
-} else {
-    console.log('El número es menor o igual a 18: ' + nroDeseado);
-}
+    }
+     else if (nroDeseado === 0) {
+    console.log('Ingrese un número mayor a 0'  + nroDeseado);
+    }
+    else {
+    console.log('El número es menor a 18: ' + nroDeseado);
+    };
